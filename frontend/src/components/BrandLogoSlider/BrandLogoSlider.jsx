3949547ee0343539bfea2edba5cb171c8fd89356
razzivofficial <razzivofficial@gmail.com>
2024-02-29 17:20:47 +0530

    brand-logo-slider-working

--- frontend/src/components/BrandLogoSlider/BrandLogoSlider.jsx
+++ frontend/src/components/BrandLogoSlider/BrandLogoSlider.jsx
@@ -1,61 +1,48 @@
-import React, { useState, useEffect } from "react";
-import { Box, Center } from "@chakra-ui/react";
-import "./testing.css";
+import React from "react";
+import { Box, ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { keyframes } from "@emotion/react";
 
-const BarndLogoSlider = () => {
-  const logos = [
-    "https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg",
-    "https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg",
-    "https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg",
-    "https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg",
-    "https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg",
-    "https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg",
-    "https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg",
-  ];
+const animationSpeed = "40s";
 
-  const [currentLogo, setCurrentLogo] = useState(0);
+const scroll = keyframes`
+  0% { transform: translateX(0); }
+  100% { transform: translateX(calc(-250px * 7))}
+`;
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentLogo((prev) => (prev + 1) % logos.length);
-    }, 3000); // Adjust the time interval as needed (in milliseconds)
-
-    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
-  }, [logos.length]);
-
+const ImageSlider = () => {
   return (
-    <Box textAlign="center" py={8}>
-      <h2>Our Partners</h2>
-      <Center>
+    <ChakraProvider>
+      <CSSReset />
+      <Box
+        height="100px"
+        margin="auto"
+        overflow="hidden"
+        position="relative"
+        width="auto"
+        className="slider"
+      >
         <Box
-          maxW="900px"
-          mx="auto"
-          overflow="hidden"
-          borderRadius="md"
-          boxShadow="md"
-          borderWidth="1px"
-          position="relative"
+          display="flex"
+          width="calc(250px * 14)"
+          css={{
+            animation: `${scroll} ${animationSpeed} linear infinite`,
+          }}
+          className="slide-track"
         >
-          <Box
-            display="flex"
-            transition="transform 0.5s ease-in-out"
-            transform={`translateX(-${currentLogo * 100}%)`}
-            width={`${logos.length * 100}%`}
-          >
-            {logos.map((logo, index) => (
-              <Box key={index} flex="0 0 auto" width="100%">
-                <img
-                  src={logo}
-                  alt={`Logo ${index + 1}`}
-                  style={{ width: "100%", height: "auto" }}
-                />
-              </Box>
-            ))}
-          </Box>
+          {[1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7].map((index) => (
+            <Box key={index} height="100px" width="250px" className="slide">
+              <img
+                src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/${index}.png`}
+                height="100"
+                width="250"
+                alt=""
+              />
+            </Box>
+          ))}
         </Box>
-      </Center>
-    </Box>
+      </Box>
+    </ChakraProvider>
   );
 };
 
-export default BarndLogoSlider;
+export default ImageSlider;
