da26f2d56b5c29d88ae2cb8bdeecefce1f09a0ef
razzivofficial <razzivofficial@gmail.com>
2024-03-01 23:50:13 +0530

    JindGenius-page-added

--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 // import Footer from "../../components/Footer/Footer"
-
+import JindGenius from "../JindGenius/JindGenius";
 function Homepage() {
   return (
     <>
       <Navbar />
+      <JindGenius/>
     </>
   );
 }
