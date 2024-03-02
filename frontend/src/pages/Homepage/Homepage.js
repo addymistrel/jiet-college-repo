a9eeeb9d25e48179936166852eac58f033146532
razzivofficial <razzivofficial@gmail.com>
2024-03-02 14:57:30 +0530

    updating-slider

--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-// import Footer from "../../components/Footer/Footer"
+import MainSlider from "../../components/MainSlider/MainSlider";
 
 function Homepage() {
   return (
     <>
       <Navbar />
+      <MainSlider />
       <Footer />
     </>
   );
