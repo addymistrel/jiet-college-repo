8f1d6f929eb97913d508fe5807d57312ebb48c7f
razzivofficial <razzivofficial@gmail.com>
2024-03-07 19:23:50 +0530

    leadership-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,8 @@ import Footer from "./components/Footer/Footer";
 import Error from "./pages/Error/error";
 import MissionAndVision from "./pages/AboutUs/MissionAndVision/MissionAndVision";
 import ChairmansMessage from "./pages/AboutUs/ChairmansMessage/ChairmansMessage";
-<<<<<<< HEAD
 import Leadership from "./pages/AboutUs/Leadership/Leadership";
-=======
 import PostGraduate from "./pages/Departments/PostGraduate/Postgraduate";
->>>>>>> ca77c7c50b54e537a392dfa661f3be44ab0c1467
 
 function App() {
   return (
@@ -23,11 +20,8 @@ function App() {
         <Route path="/aboutus/overview" element={<Overview />} />
         <Route path="/aboutus/cmessage" element={<ChairmansMessage />} />
         <Route path="/aboutus/mission" element={<MissionAndVision />} />
-<<<<<<< HEAD
         <Route path="/aboutus/leadership" element={<Leadership />} />
-=======
         <Route path="/programs/postgraduate" element={<PostGraduate />} />
->>>>>>> ca77c7c50b54e537a392dfa661f3be44ab0c1467
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
